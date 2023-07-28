032c7e6facf663d7555da06f856a92053a453a1b
mesinkasir <52827888+mesinkasir@users.noreply.github.com>
2023-07-28 20:11:04 +0700

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,94 @@
+const { DateTime } = require('luxon');
+const fs = require('fs');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const { eleventyImagePlugin } = require('@11ty/eleventy-img');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+  eleventyConfig.addPlugin(eleventyImagePlugin, {
+    formats: ['webp', 'jpeg', 'avif'],
+    urlPath: '/img/',
+    defaultAttributes: {
+      loading: 'lazy',
+      decoding: 'async',
+    },
+  });
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy'
+    );
+  });
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+  eleventyConfig.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
+  eleventyConfig.addFilter('filterTagList', (tags) => {
+    return (tags || []).filter(
+      (tag) => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1
+    );
+  });
+  eleventyConfig.addCollection('tagList', function (collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach((item) => {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+    });
+    return [...tagSet];
+  });
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('css');
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
+  });
+  eleventyConfig.setLibrary('md', markdownLibrary);
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html');
+
+        browserSync.addMiddleware('*', (req, res) => {
+          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' });
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false,
+  });
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    pathPrefix: '/',
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: false,
+    dir: {
+      input: '.',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
+    },
+  };
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Cloudcannon 11ty simple starter
+
+Integration cloudcannon cms with eleventy 11ty
+
+on progress.....
